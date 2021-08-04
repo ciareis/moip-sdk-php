48c37e72c7c169c6f28437e6f1ada9c5137cf997
Leandro Henrique <emtudo@gmail.com>
2021-08-04 13:45:25 -0300

    fix: paid

--- src/Resource/Orders.php
+++ src/Resource/Orders.php
@@ -148,7 +148,7 @@ class Orders extends MoipResource
         $this->orders->data->ownId = $response->ownId;
         $this->orders->data->createdAt = $response->createdAt;
         $this->orders->data->updatedAt = $response->updatedAt;
-        $this->orders->data->amount->paid = $response->amount->paid;
+        $this->orders->data->amount->paid = $response->amount->paid ?? null;
         $this->orders->data->amount->total = $response->amount->total;
         $this->orders->data->amount->fees = $response->amount->fees;
         $this->orders->data->amount->refunds = $response->amount->refunds;
